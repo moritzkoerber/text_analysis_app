89d7344d4fc586b5221c031f1816534d66d3d127
moritzkoerber <moritz.koerber@tum.de>
2019-08-30 15:19:35 +0200

    app2

--- app/run.py
+++ app/run.py
@@ -9,7 +9,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar, hist
+from plotly.graph_objs import Bar, Histogram
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
